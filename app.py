133a7f77a5ec478eb7e8334573f74287ba785f64
Noxan <59417659+noxan-dev@users.noreply.github.com>
2022-06-30 15:15:05 -0400

    send data to be used, sessions

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, jsonify, session
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_bootstrap import Bootstrap5
-from forms import GardenBedSize, PlantForm
+from forms import GardenBedSize
 from flask_caching import Cache
+from flask_wtf.csrf import CSRFProtect
+from werkzeug.datastructures import ImmutableMultiDict
+import json
 
 cache = Cache(config={'CACHE_TYPE': 'simple'})
+csrf = CSRFProtect()
 
 app = Flask(__name__)
 
 cache.init_app(app)
+csrf.init_app(app)
+
 app.config['TESTING'] = True
 app.config['SECRET_KEY'] = 'mysecretkey'
 TEMPLATES_AUTO_RELOAD = True
@@ -19,34 +25,48 @@ bootstrap = Bootstrap5(app)
 @cache.cached(timeout=60)
 def index():
     garden_bed_form = GardenBedSize()
-    plant_form = PlantForm()
-    return render_template('index.html', garden_bed_form=garden_bed_form, plant_form=plant_form)
+    return render_template('index.html', garden_bed_form=garden_bed_form)
 
 
 @app.route('/config', methods=['GET', 'POST'])
 def config():
     garden_bed_form = GardenBedSize()
-    plant_form = PlantForm()
+
+    data = json.loads(open('data.json').read())
 
     if request.method == 'POST':
+        print(request.form)
         if garden_bed_form.submit.data and garden_bed_form.validate():
             flash('Garden bed size: {} x {}'.format(garden_bed_form.width.data, garden_bed_form.length.data))
             session['width'] = garden_bed_form.width.data
             session['length'] = garden_bed_form.length.data
             return redirect(url_for('config'))
-        elif plant_form.submit.data and plant_form.validate():
-            flash('Plants: {}'.format(plant_form.plant.data))
-            session['plants'] = plant_form.plant.data
-            return redirect(url_for('config'))
-    return render_template('config.html', garden_bed_form=garden_bed_form, plant_form=plant_form)
+    return render_template('config.html', garden_bed_form=garden_bed_form, data=data)
 
 
 @app.route('/mygarden', methods=['GET', 'POST'])
 def my_garden():
+    area = session.get('width') * session.get('length')
+    my_garden_dict = {'width': session.get('width'),
+                      'length': session.get('length'),
+                      'area': area,
+                      'plants': session.get('plants')
+                      }
+    if 'plant_form' in request.form:
+        session['plants'] = {}
+        flash('Plants: {}'.format(request.form.get('plant')))
+        plant_form = request.form.to_dict()
+        plant_form_values = plant_form.values()
+        plant_form_keys = plant_form.keys()
 
-    return render_template('mygarden.html')
+        for key in plant_form_keys:
+            for plant in plant_form_values:
+                if key is None:
+                    continue
+                if key not in session['plants']:
+                    session['plants'][key] = plant
+    return render_template('mygarden.html', my_garden=my_garden_dict)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
--- forms.py
+++ forms.py
@@ -1,26 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, IntegerField, FieldList, Label
+from wtforms import StringField, SubmitField, IntegerField
 from wtforms.validators import DataRequired, NumberRange
-import json
-
-data = json.loads(open('data.json').read())
 
 
 class GardenBedSize(FlaskForm):
     width = IntegerField('Width', validators=[DataRequired(), NumberRange(min=0, max=100)], render_kw={'class': 'form-control'})
     length = IntegerField('Length', validators=[DataRequired(), NumberRange(min=0, max=100)], render_kw={'class': 'form-control'})
     submit = SubmitField('Submit', render_kw={'class': 'btn btn-primary'})
-
-    def __init__(self, *args, **kwargs):
-        super(GardenBedSize, self).__init__(*args, **kwargs)
-        self.width.data = 0
-        self.length.data = 0
-
-
-class PlantForm(FlaskForm):
-    for item in data:
-        plant = data.get(item)
-        plant_field = FieldList(label='Plants', min_entries=1, max_entries=None, render_kw={'class': 'form-control'}, unbound_field=StringField(label=plant['name'], render_kw={'class': 'form-control'}))
-    num_plants = IntegerField('Number of plants', validators=[DataRequired(), NumberRange(min=0, max=100)])
-    image = SubmitField('Add Image')
-    submit = SubmitField('Submit')
